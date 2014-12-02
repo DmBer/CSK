168c82f23de753575321fe1d774c7ebb5c1cb262
dimax <dimax2008@ukr.net>
2014-12-02 13:04:02 +0200

    AddNewConsoleWriteLineandReadKey

--- CSKVideo/CSKVideo/Program.cs
+++ CSKVideo/CSKVideo/Program.cs
@@ -11,6 +11,8 @@ namespace CSKVideo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello");
+            Console.ReadKey();
         }
     }
 }
