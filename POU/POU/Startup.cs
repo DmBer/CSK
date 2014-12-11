a51bd24c6fa3e0bafb368ffd316840a725833818
dimax <dimax2008@ukr.net>
2014-12-11 11:09:51 +0200

    111

---
+++ POU/POU/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(POU.Startup))]
+namespace POU
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
